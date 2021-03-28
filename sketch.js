5b821e37595cbd682ccfa135001c829d411ed253
santhosh776 <77959315+santhosh776@users.noreply.github.com>
2021-03-28 10:51:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -50,6 +50,34 @@ async function getBackgroundImg(){
     {
         bg = "sunrise2.png"
     }
+    else if(hour>=08 && hour<=10)
+    {
+        bg = "sunrise3.png"
+    }
+    else if(hour>=10 && hour<=12)
+    {
+        bg = "sunrise4.png"
+    }
+    else if(hour>=12 && hour<=14)
+    {
+        bg = "sunrise5.png"
+    }
+    else if(hour>=14 && hour<=16)
+    {
+        bg = "sunrise6.png"
+    }
+    else if(hour>=16 && hour<=18)
+    {
+        bg = "sunset7.png"
+    }
+    else if(hour>=18 && hour<=20)
+    {
+        bg = "sunset8.png"
+    }
+    else if(hour>=20 && hour<=23)
+    {
+        bg = "sunset9.png"
+    }
     else if(hour>=23 && hour==0)
     {
         bg = "sunset10.png"
